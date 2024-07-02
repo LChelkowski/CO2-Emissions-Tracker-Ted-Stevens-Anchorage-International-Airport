a066da93fa621f30da7094504c1bb8a96e3b39c6
LChelkowski <@lachelkowski@gmail.com>
2024-07-01 21:01:12 -0400

    Update start.sh to correct the path to the Panel script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-panel serve app.py --address=0.0.0.0 --port=10000 --allow-websocket-origin=co2-emissions-tracker-ted-stevens.onrender.com
+panel serve Panel..py --address=0.0.0.0 --port=10000 --allow-websocket-origin=co2-emissions-tracker-ted-stevens.onrender.com
