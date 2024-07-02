d33f1ee43d48a372ccabc847b930fa06d0297cd6
LChelkowski <@lachelkowski@gmail.com>
2024-07-02 14:45:08 -0400

    Update path in start.sh script to reflect correct directory structure

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-panel serve /opt/render/project/src/Panel.py --address=0.0.0.0 --port=10000 --allow-websocket-origin=co2-emissions-tracker-ted-stevens.onrender.com
+panel serve /opt/render/project/src/PanelTest.py --address=0.0.0.0 --port=10000 --allow-websocket-origin=co2-emissions-tracker-ted-stevens.onrender.com
