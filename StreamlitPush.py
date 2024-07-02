db375752cc74d709b8e87f69c0d94f627ed4a316
LChelkowski <@lachelkowski@gmail.com>
2024-07-01 20:35:57 -0400

    Update StreamlitPush.py

--- StreamlitPush.py
+++ StreamlitPush.py
@@ -1,3 +1,14 @@
-import os 
+import os
+import subprocess
+import time
 
-os.system('panel serve panel.py --address=0.0.0.0 --port=8501') 
+# Start the Panel server in a subprocess
+panel_process = subprocess.Popen(
+    ['panel', 'serve', 'Panel.py', '--address=0.0.0.0', '--port=8501']
+)
+
+# Give the Panel server some time to start
+time.sleep(5)
+
+# Notify Streamlit to load the Panel app
+st.markdown(f'<iframe src="http://localhost:8501" width="100%" height="800px"></iframe>', unsafe_allow_html=True)
